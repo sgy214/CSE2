278ccbbf9be98994c7be4d0843385598db58366f
sgy214 <sgy214@lehigh.edu>
2014-08-27 16:11:47 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//  Sunny Yu
+//  hw01
+//  WelcomeClass Java Program
+
+//  define a class
+public class WelcomeClass
+{
+    //  add main method
+    public static void main(String[] args) 
+    {
+        // print the welcome message and Lehigh ID
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-S--G--Y--2--1--4-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        
+        // print the autobiographical statement
+        System.out.println("I was originally born in Canada and recieved my American citizenship last year.");
+    }
+}
